f5e960d66edf5fe4fd5fec24054ae98622dcfab3
Melvin Lang <121934188+GithubRealFan@users.noreply.github.com>
2025-11-23 18:09:50 -0800

    Update url.js

--- url.js
+++ url.js
@@ -1,26 +1,3 @@
-RD CFAR :
-https://github.com/colindecourt/darod
-https://github.com/yizhou-wang/RODNet
-https://arxiv.org/pdf/2208.02474
-https://arxiv.org/pdf/2505.10150
-https://arxiv.org/pdf/2005.01456
-https://arxiv.org/pdf/2208.02474
-https://github.com/colindecourt/darod
-https://link.springer.com/article/10.1007/s11554-023-01316-5
-https://github.com/ZHOUYI1023/awesome-radar-perception
-https://arxiv.org/html/2407.03140v1
-https://arxiv.org/html/2509.18068
-
-SFP Cage :
-https://cdn.amphenol-cs.com/media/wysiwyg/files/documentation/datasheet/inputoutput/hsio_cn_ultraport_sfp.pdf
-
-F-Tone :
-https://www.fs.com/uk/products/36436.html?now_cid=1113
-
-High Speed Curcuit :
-https://www.ti.com/lit/an/slla335/slla335.pdf
-https://resources.altium.com/p/gigabit-ethernet-switch-project
-https://github.com/PacktPublishing/Architecting-and-Building-High-Speed-SoCs
-https://github.com/ogurasek123/6t-sram-cell-cmos-design-electric-vlsi-tool
-https://www.protoexpress.com/blog/best-high-speed-pcb-routing-practices/
-https://www.army-technology.com/projects/krasukha-electronic-warfare-ew-system-russia/?cf-view
+https://spaceforest.pl/hussar-radar-series/hussar-drone/
+https://www.researchgate.net/publication/224713393_The_BYU_mu_SAR_A_Small_Student-Built_SAR_for_UAV_Operation
+https://www.mdpi.com/1424-8220/13/3/3204
