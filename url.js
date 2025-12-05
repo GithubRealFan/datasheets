8185a5c56a238c463e05a56cdda6e000b4e499fc
Melvin Lang <121934188+GithubRealFan@users.noreply.github.com>
2025-12-04 20:14:44 -0800

    Update url.js

--- url.js
+++ url.js
@@ -1,3 +1,6 @@
 https://spaceforest.pl/hussar-radar-series/hussar-drone/
 https://www.researchgate.net/publication/224713393_The_BYU_mu_SAR_A_Small_Student-Built_SAR_for_UAV_Operation
 https://www.mdpi.com/1424-8220/13/3/3204
+
+https://www.luxnetcorp.com.tw/storage/architecture/product~126/product~127/MI5D-903G-KS2_1-1.pdf
+https://eoptolink.com/pdf/LC-TOSA-CWDM.pdf
